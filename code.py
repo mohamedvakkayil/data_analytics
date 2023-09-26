dfe2495cb390bc5b083a7907e9d88c929d5d426d
MOHAMED VAKKAYIL <mohamedvakkayil@mohameds-air-2.home>
2023-09-26 17:48:44 -0400

    second commit

---
+++ code.py
@@ -0,0 +1,17 @@
+'''Objectives: 
+•	Apply principles and practices of data exploration, manipulation and preprocessing
+•	Program at a basic level using R programming language
+•	Explore data analytics techniques such as correlation, visualization such as scatter plots,
+ pie chart, table, distribution of data, and outlier analysis.
+Requirements: 
+1.	Create a new single data set that can be used to output a table that lists the number of persons injured 
+or killed in traffic accidents in each neighbourhood of Toronto in the last 4 years. (You will need to extract 
+the following information: Year, vehicles in the street, district, and Neighbourhood)
+2.	Write a sequence of code that shows the total vehicles in the street for each district for the last 4 
+years during the accidents.
+3.	Write a sequence of code that lists the top 5 neighbourhoods with the highest average number of 
+vehicles in the streets.
+'''
+
+import pandas as pd
+print(pd.__version__)
