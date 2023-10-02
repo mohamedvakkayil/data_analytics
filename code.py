ee8ee0231125dd64f679688fdb8622cb17c3f79a
MOHAMED VAKKAYIL <mohamedvakkayil@MOHAMEDs-MacBook-Air-2.local>
2023-10-01 23:20:42 -0400

    jupyter and consolidated

--- code.py
+++ code.py
@@ -1,25 +1,25 @@
-'''Objectives: 
-•	Apply principles and practices of data exploration, manipulation and preprocessing
-•	Program at a basic level using R programming language
-•	Explore data analytics techniques such as correlation, visualization such as scatter plots,
- pie chart, table, distribution of data, and outlier analysis.
-Requirements: 
-1.	Create a new single data set that can be used to output a table that lists the number of persons injured 
-or killed in traffic accidents in each neighbourhood of Toronto in the last 4 years. (You will need to extract 
-the following information: Year, vehicles in the street, district, and Neighbourhood)
-2.	Write a sequence of code that shows the total vehicles in the street for each district for the last 4 
-years during the accidents.
-3.	Write a sequence of code that lists the top 5 neighbourhoods with the highest average number of 
-vehicles in the streets.
-'''
+# Objectives: 
+# •	Apply principles and practices of data exploration, manipulation and preprocessing
+# •	Program at a basic level using R programming language
+# •	Explore data analytics techniques such as correlation, visualization such as scatter plots,
+#  pie chart, table, distribution of data, and outlier analysis.
+# Requirements: 
+# 1.	Create a new single data set that can be used to output a table that lists the number of persons injured 
+# or killed in traffic accidents in each neighbourhood of Toronto in the last 4 years. (You will need to extract 
+# the following information: Year, vehicles in the street, district, and Neighbourhood)
+# 2.	Write a sequence of code that shows the total vehicles in the street for each district for the last 4 
+# years during the accidents.
+# 3.	Write a sequence of code that lists the top 5 neighbourhoods with the highest average number of 
+# vehicles in the streets.
+
 
 import pandas as pd
 
 pd.set_option('display.max_colwidth', None) 
 
-'''1.	Create a new single data set that can be used to output a table that lists the number of persons injured 
-or killed in traffic accidents in each neighbourhood of Toronto in the last 4 years. (You will need to extract 
-the following information: Year, vehicles in the street, district, and Neighbourhood)'''
+# '''1.	Create a new single data set that can be used to output a table that lists the number of persons injured 
+# or killed in traffic accidents in each neighbourhood of Toronto in the last 4 years. (You will need to extract 
+# the following information: Year, vehicles in the street, district, and Neighbourhood)'''
 
 column_mapping = [
     'YEAR',
@@ -86,3 +86,6 @@ df.columns = column_mapping
 df_new= pd.concat([df_new, df])
 
 print(df_new)
+
+# 2.	Write a sequence of code that shows the total vehicles in the street for each district for the last 4 
+# years during the accidents.
